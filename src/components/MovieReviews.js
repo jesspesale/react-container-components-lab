99426e6f0f5e90844356ce8bcdf59daf84b69146
Jess Pesale <jess_pesale@aol.com>
2021-10-25 21:24:14 -0400

    commit tree 9752b11a7acd1bc02ebd083d62b4cf4db39c2b91

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from 'react'
+
+
+const MovieReviews = ({reviews}) => (
+    <div className="review-list" >
+        {reviews.map(review => (
+            //  console.log(review),
+            <div className="review" key={review.display_title}>
+            <h4>{review.display_title}</h4>
+            <h5>By: {review.byline}</h5>
+            <h5>Critics Pick: {review.critics_pick}</h5>
+            </div>
+        ))}
+    </div>
+)
+
+export default MovieReviews
